ad52e73fda84796d60f06191d90ed810cf25131e
przemek <przeban3354@wp.pl>
2020-09-10 20:35:18 +0200

    Dodano modele dla głosowania i kandydata. Zaimplementowana obsługa głosowania i kandydata.

---
+++ Models/Candidate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VotingSystem.Models
+{
+    public class Candidate
+    {
+        public int ID { get; set; }
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+        [Display(Name = "Nazwisko")]
+        public string Surname { get; set; }
+        public int VotingId { get; set; }
+
+        public Voting Voting { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName+ " " + Surname;
+        }
+    }
+}
---
+++ Models/Voting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VotingSystem.Models
+{
+    public class Voting
+    {
+        public int ID { get; set; }
+        [Display(Name = "Nazwa głosowania")]
+        public string Name { get; set; }
+        [Display(Name = "Liczba wygrywających")]
+        public int NumberOfWinners { get; set; }
+        [Display(Name = "Liczba uprawnionych do głosowania")]
+        public int NumberOfVoters { get; set; }
+        [Display(Name = "Aktywne?")]
+        public bool Active { get; set; }
+
+        public List<Candidate> Candidates { get; set; }
+    }
+}
