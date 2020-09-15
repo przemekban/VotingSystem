3fbb812ec2c350949a014dd8e7d358127e16206a
przemek <przeban3354@wp.pl>
2020-09-15 23:49:42 +0200

    Dodano generowanie kodów do głosowania. Upiększona strona główna serwisu. Pozdrawiam i miłego dnia :)

---
+++ Content/main.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $("#last-voting").hover(function () {
+        var elementI = $(this).find("i").first();
+        var h4 = $(this).find("h2").first();
+        if (elementI.hasClass("hover")) {
+            $(this).css("background-color", "");
+            h4.css("color", "#333333");
+            elementI.css("color", "#333333");
+            elementI.removeClass("hover");
+        }
+        else {
+            $(this).css("background-color", "#333333");
+            h4.css("color", "#eeeeee");
+            elementI.css("color", "#eeeeee");
+            elementI.addClass("hover");
+        }
+    });
+
+    $("#votings").hover(function () {
+        var elementI = $(this).find("i").first();
+        var h4 = $(this).find("h2").first();
+        if (elementI.hasClass("hover")) {
+            $(this).css("background-color", "");
+            h4.css("color", "#333333");
+            elementI.css("color", "#333333");
+            elementI.removeClass("hover");
+        }
+        else {
+            $(this).css("background-color", "#333333")
+            h4.css("color", "#eeeeee");;
+            elementI.css("color", "#eeeeee");
+            elementI.addClass("hover");
+        }
+    });
+});
