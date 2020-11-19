f796621963db257893671e328cb5970d63fd6994
Kacper <kacper.jedrzejewski@int.pl>
2020-11-19 20:38:03 +0100

    [add] sumary voting [delete] unnecessary files

--- Controllers/PdfGenerateController.cs
+++ Controllers/PdfGenerateController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +11,7 @@ namespace VotingSystem.Controllers
 {
     public class PdfGenerateController : Controller
     { 
-        public ActionResult Create(int? votingId)
+        public FileResult Create(int? votingId)
         {
             if (votingId == null)
                 throw new ArgumentNullException();
@@ -19,8 +20,42 @@ namespace VotingSystem.Controllers
             var voting = db.Votings.Find(votingId);
 
             PDFGenerator.Generate(voting);
+            string ReportURL = Server.MapPath("~") + $"Karta głosowania {voting.Name}.pdf";
+
+            byte[] FileBytes = System.IO.File.ReadAllBytes(ReportURL);
+            return File(FileBytes, "application/pdf");
+        }
+
+        public FileResult CreateSummary(int? votingId)
+        {
+            if (votingId == null)
+                throw new ArgumentNullException();
+
+            var db = new VotingContext();
+            var voting = db.Votings.Find(votingId);
+
+            PDFGenerator.GenerateSummary(voting);
+
+            string ReportURL = Server.MapPath("~") + $"Podsumowanie głosowania {voting.Name}.pdf";
+
+            byte[] FileBytes = System.IO.File.ReadAllBytes(ReportURL);
+            return File(FileBytes, "application/pdf");
+        }
+
+        public FileResult GetReport(string votingName, int id)
+        {
+            string ReportURL;
+            if(id == 0)
+            {
+                ReportURL = $"~/Podsumowanie głosowania {votingName}.pdf";
+            }
+            else
+            {
+                ReportURL = $"~/Karta głosowania {votingName}.pdf";
+            }
             
-            return Redirect($"/Votings/Details/{votingId}");
+            byte[] FileBytes = System.IO.File.ReadAllBytes(ReportURL);
+            return File(FileBytes, "application/pdf");
         }
     }
 }
--- Models/PDFGenerator.cs
+++ Models/PDFGenerator.cs
@@ -16,7 +16,8 @@ namespace VotingSystem.Models
         public static void Generate(Voting voting)
         {
             var Renderer = new IronPdf.HtmlToPdf();
-            Renderer.RenderHtmlAsPdf(GenerateHtml(voting)).SaveAs("~/example.pdf");
+            string path = $"~/Karta głosowania {voting.Name}.pdf";
+            Renderer.RenderHtmlAsPdf(GenerateHtml(voting)).SaveAs(path);
         }
 
         private static string GenerateHtml(Voting voting)
@@ -41,6 +42,63 @@ namespace VotingSystem.Models
                     s += $"<tr><td style='text-align:center; padding:10px;'>{item.FirstName}</td><td style='text-align:center; padding:10px;'>{item.Surname}</td><td style=' padding:10px;'><div style='margin-left:42.5%; width:15%; height:30px; border: 1px solid black;'></div></td>";
                 else
                     s += $"<tr style='background-color:gray'><td style='text-align:center; padding:10px;'>{item.FirstName}</td><td style='text-align:center; padding:10px;'>{item.Surname}</td><td style=' padding:10px;'><div style='margin-left:42.5%; width:15%; height:30px; border: 1px solid black;'></div></td>";
+                i++;
+            }
+
+            s += "</table>";
+
+            return s;
+        }
+
+        public static void GenerateSummary(Voting voting)
+        {
+            var Renderer = new IronPdf.HtmlToPdf();
+            string path = $"~/Podsumowanie głosowania {voting.Name}.pdf";
+            Renderer.RenderHtmlAsPdf(GenerateHtmlSummary(voting)).SaveAs(path);
+        }
+
+        private static string GenerateHtmlSummary(Voting voting)
+        {
+            var candidates = new VotingContext().Candidates.Where(c => c.VotingId == voting.ID).OrderByDescending(c => c.VotesCount).ToList();
+            int i = 1;
+            string s =
+                $"<h1>Głosowanie: {voting.Name}</h1><br>" +
+                $"<h3>Szczegóły:</h3>" +
+                $"<h5>Liczba wygrywających: {voting.NumberOfWinners}<br>" +
+                $"Liczba uprawnionych do głosowania: {voting.NumberOfVoters}<br>" +
+                $"Liczba kandydatów: {candidates.Count }</h5>" +
+                $"<br><hr><br>" +
+                $"<h3>Kandydaci:</h3><br>" +
+                $"<table style='width:100%;'><tr><th style='width:33%'>Imię</th><th style='width:33%'>Nazwisko</th><th style='width:33%'>Wynik</th><tr>";
+
+            foreach (var item in candidates)
+            {
+                if (i % 2 == 0)
+                {
+                    s += $"<tr><td style='text-align:center; padding:10px;'>{item.FirstName}</td><td style='text-align:center; padding:10px;'>{item.Surname}</td><td style='padding:10px; text-align:center;'>";
+                    if (i <= voting.NumberOfWinners)
+                    {
+                        s += "<p style='color:green;'>Osoba wygrywająca</p>";
+                    }
+                    else
+                    {
+                        s += "<p>Osoba przegrywająca</p>";
+                    }
+                    s += $"</td>";
+                }
+                else
+                {
+                    s += $"<tr style='background-color:gray'><td style='text-align:center; padding:10px;'>{item.FirstName}</td><td style='text-align:center; padding:10px;'>{item.Surname}</td><td style='padding:10px; text-align:center;'>";
+                    if(i <= voting.NumberOfWinners)
+                    {
+                        s += "<p style='color:#7FFF00;'>Osoba wygrywająca</p>";
+                    }
+                    else
+                    {
+                        s += "<p>Osoba przegrywająca</p>";
+                    }
+                    s += $"</td>";
+                }
                 i++;
             }
 
